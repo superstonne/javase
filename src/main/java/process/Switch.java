aa05c79bf2d1136fee88bfbc01ad451a17df317e
Nick He <515122644@qq.com>
2018-11-19 22:51:16 +0800

    process

---
+++ src/main/java/process/Switch.java
@@ -0,0 +1,16 @@
+package process;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Administrator on 2018\11\19 0019.
+ */
+public class Switch {
+
+    public static void main(String[] args) {
+        String str = "";
+        switch (1) {
+            case 1: break;
+        }
+    }
+}
